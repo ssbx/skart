81113c5a3ebd53497d424772ec0f36ad44808d59
Sébastien Serre <sserre.bx@gmail.com>
2016-03-01 21:58:48 +0100

    shaders test

--- code/ex3.c
+++ code/ex3.c
@@ -5,90 +5,98 @@
 #include "stdio.h"
 #include "math.h"
 
-
 static GLuint program_id;
 static const GLfloat g_vertex_buffer_data[] = {
     -1.0f,-1.0f,-1.0f, // triangle 1 : begin
     -1.0f,-1.0f, 1.0f,
     -1.0f, 1.0f, 1.0f, // triangle 1 : end
-    1.0f, 1.0f,-1.0f, // triangle 2 : begin
+     1.0f, 1.0f,-1.0f, // triangle 2 : begin
     -1.0f,-1.0f,-1.0f,
     -1.0f, 1.0f,-1.0f, // triangle 2 : end
-    1.0f,-1.0f, 1.0f,
+     1.0f,-1.0f, 1.0f,
     -1.0f,-1.0f,-1.0f,
-    1.0f,-1.0f,-1.0f,
-    1.0f, 1.0f,-1.0f,
-    1.0f,-1.0f,-1.0f,
+     1.0f,-1.0f,-1.0f,
+     1.0f, 1.0f,-1.0f,
+     1.0f,-1.0f,-1.0f,
     -1.0f,-1.0f,-1.0f,
     -1.0f,-1.0f,-1.0f,
     -1.0f, 1.0f, 1.0f,
     -1.0f, 1.0f,-1.0f,
-    1.0f,-1.0f, 1.0f,
+     1.0f,-1.0f, 1.0f,
     -1.0f,-1.0f, 1.0f,
     -1.0f,-1.0f,-1.0f,
     -1.0f, 1.0f, 1.0f,
     -1.0f,-1.0f, 1.0f,
-    1.0f,-1.0f, 1.0f,
-    1.0f, 1.0f, 1.0f,
-    1.0f,-1.0f,-1.0f,
-    1.0f, 1.0f,-1.0f,
-    1.0f,-1.0f,-1.0f,
-    1.0f, 1.0f, 1.0f,
-    1.0f,-1.0f, 1.0f,
-    1.0f, 1.0f, 1.0f,
-    1.0f, 1.0f,-1.0f,
+     1.0f,-1.0f, 1.0f,
+     1.0f, 1.0f, 1.0f,
+     1.0f,-1.0f,-1.0f,
+     1.0f, 1.0f,-1.0f,
+     1.0f,-1.0f,-1.0f,
+     1.0f, 1.0f, 1.0f,
+     1.0f,-1.0f, 1.0f,
+     1.0f, 1.0f, 1.0f,
+     1.0f, 1.0f,-1.0f,
     -1.0f, 1.0f,-1.0f,
-    1.0f, 1.0f, 1.0f,
+     1.0f, 1.0f, 1.0f,
     -1.0f, 1.0f,-1.0f,
     -1.0f, 1.0f, 1.0f,
-    1.0f, 1.0f, 1.0f,
+     1.0f, 1.0f, 1.0f,
     -1.0f, 1.0f, 1.0f,
-    1.0f,-1.0f, 1.0f
+     1.0f,-1.0f, 1.0f
 };
 
+
+#define COL1 1.0f, 0.0f, 0.0f,
+#define COL2 1.0f, 0.2f, 0.0f,
+#define COL3 1.0f, 0.4f, 0.0f,
+#define COLEND 1.0f, 0.2f, 0.0f
+
 static const GLfloat g_color_buffer_data[] = {
-    0.583f,  0.771f,  0.014f,
-    0.609f,  0.115f,  0.436f,
-    0.327f,  0.483f,  0.844f,
-    0.822f,  0.569f,  0.201f,
-    0.435f,  0.602f,  0.223f,
-    0.310f,  0.747f,  0.185f,
-    0.597f,  0.770f,  0.761f,
-    0.559f,  0.436f,  0.730f,
-    0.359f,  0.583f,  0.152f,
-    0.483f,  0.596f,  0.789f,
-    0.559f,  0.861f,  0.639f,
-    0.195f,  0.548f,  0.859f,
-    0.014f,  0.184f,  0.576f,
-    0.771f,  0.328f,  0.970f,
-    0.406f,  0.615f,  0.116f,
-    0.676f,  0.977f,  0.133f,
-    0.971f,  0.572f,  0.833f,
-    0.140f,  0.616f,  0.489f,
-    0.997f,  0.513f,  0.064f,
-    0.945f,  0.719f,  0.592f,
-    0.543f,  0.021f,  0.978f,
-    0.279f,  0.317f,  0.505f,
-    0.167f,  0.620f,  0.077f,
-    0.347f,  0.857f,  0.137f,
-    0.055f,  0.953f,  0.042f,
-    0.714f,  0.505f,  0.345f,
-    0.783f,  0.290f,  0.734f,
-    0.722f,  0.645f,  0.174f,
-    0.302f,  0.455f,  0.848f,
-    0.225f,  0.587f,  0.040f,
-    0.517f,  0.713f,  0.338f,
-    0.053f,  0.959f,  0.120f,
-    0.393f,  0.621f,  0.362f,
-    0.673f,  0.211f,  0.457f,
-    0.820f,  0.883f,  0.371f,
-    0.982f,  0.099f,  0.879f
+    COL1
+    COL2
+    COL3
+    COL1
+    COL2
+    COL3
+    COL1
+    COL2
+    COL3
+    COL1
+    COL2
+    COL3
+    COL1
+    COL2
+    COL3
+    COL1
+    COL2
+    COL3
+    COL1
+    COL2
+    COL3
+    COL1
+    COL2
+    COL3
+    COL1
+    COL2
+    COL3
+    COL1
+    COL2
+    COL3
+    COL1
+    COL2
+    COL3
+    COL1
+    COL2
+    COLEND
 };
+
 static GLuint vertexbuffer;
 static GLuint colorbuffer;
 static GLuint IN_modelVertex;
 static GLuint IN_colorVertex;
 static GLuint IN_modelViewProjection;
+static GLuint IN_sinVar;
+
 static Mat4 projection;
 static Mat4 view;
 static Mat4 model;
@@ -106,13 +114,17 @@ void ex3_init()
     IN_modelViewProjection =
         glGetUniformLocation(program_id, "modelViewProjection");
 
+    IN_sinVar =
+        glGetUniformLocation(program_id, "sinVar");
+
     IN_modelVertex =
         glGetAttribLocation(program_id, "modelVertex");
 
     IN_colorVertex =
         glGetAttribLocation(program_id, "vertexColor");
 
-    projection = perspective(45, (GLfloat) 4/3, 0.1, 100);
+
+    projection = perspective(45, (GLfloat) 4/3, 0.1, 100.0);
 
     Vec3 eye    = {4,3,3};
     Vec3 center = {0,0,0};
@@ -143,7 +155,13 @@ void ex3_update()
     // Use our shader
     glUseProgram(program_id);
 
+    float tsin = (float) sin(glfwGetTime());
+    if (tsin < 0) {
+        tsin = -tsin;
+    }
+    glUniform1f(IN_sinVar, tsin);
     glUniformMatrix4fv(IN_modelViewProjection, 1, GL_FALSE, &(MVP.a0));
+
     // 1rst attribute buffer : vertices
     glEnableVertexAttribArray(IN_modelVertex);
     glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
@@ -167,6 +185,8 @@ void ex3_update()
         0,                           // stride
         (void*)0                     // array buffer offset
     );
+
+
 
     // Draw the triangle !
     glDrawArrays(GL_TRIANGLES, 0, 12*3); // 3 indices starting at 0 -> 1 triangle
