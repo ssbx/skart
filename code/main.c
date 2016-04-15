4f1c27057ef4ceb489b138b9336c48ada0618c38
Sébastien Serre <sserre.bx@gmail.com>
2016-04-15 23:41:52 +0200

    new code layout

--- code/main.c
+++ code/main.c
@@ -37,7 +37,6 @@ int main(
     char* argv[])
 {
 
-    clogConfigure("/tmp/fed.log");
     char* windowed = cargoFlag("windowed", "FALSE", argc, argv);
     int startWindowed;
     if (strcmp(windowed, "TRUE") == 0) {
